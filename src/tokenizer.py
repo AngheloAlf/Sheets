28389f6d4d40b7206eea970acbb41cc225166b27
AngheloAlf <angheloalf95@gmail.com>
2020-04-04 18:49:14 -0300

    Tokenizer is a class

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,131 +1,130 @@
-from typing import Tuple
+from typing import List, Tuple
 import json
 
-_state_none = 0
-_special_symbols = ["$", ":", "!", "#", ","]
-_state_symbols = 1
+class Tokenizer():
+    State_None = None
+    Type_Skip = "SKP"
+    Type_Sequence = "SEQ"
+    Type_Literal = "LIT"
+    Type_Cont_Char = "CONT_CHAR"
+    Type_Cont_Token = "CONT_TKN"
+
+    def __init__(self):
+        self.registered = list()
+
+    def register_skipable(self, literal: str, identf: str, options=None):
+        data = (Tokenizer.Type_Skip, literal, identf)
+        self.registered.append(data)
+
+    def register_sequence(self, character_list: List[str], identf: str, options=None):
+        data = (Tokenizer.Type_Sequence, character_list, identf)
+        self.registered.append(data)
+
+    def register_literal(self, literal: str, identf: str, options=None):
+        data = (Tokenizer.Type_Literal, literal, identf)
+        self.registered.append(data)
+
+    def register_chars_container(self, char_container: str, identf: str, options=None):
+        data = (Tokenizer.Type_Cont_Char, char_container, identf)
+        self.registered.append(data)
+
+    def register_token_container(self, left_char_container: str, right_char_container: str, identf: str, options=None):
+        data = (Tokenizer.Type_Cont_Token, (left_char_container, right_char_container), identf)
+        self.registered.append(data)
+
+
+    def _tokenize(self, expression: str, end_char: str=""):
+        i = 0
+        result = []
+        state = Tokenizer.State_None
+        token = ""
+        while i < len(expression):
+            char = expression[i]
+            if char == end_char:
+                break
+
+            parsed = False
+            for type_, data, identf in self.registered:
+                if type_ == Tokenizer.Type_Skip:
+                    length = len(data)
+                    if expression[i:i+length] == data:
+                        if state != Tokenizer.State_None:
+                            result.append((state, token))
+                            state = Tokenizer.State_None
+                        parsed = True
+                elif type_ == Tokenizer.Type_Sequence:
+                    if char in data:
+                        if state != identf:
+                            if state != Tokenizer.State_None:
+                                result.append((state, token))
+                            token = ""
+                            state = identf
+                        token += char
+                        parsed = True
+                elif type_ == Tokenizer.Type_Literal:
+                    pass # TODO
+                elif type_ == Tokenizer.Type_Cont_Char:
+                    if char == data:
+                        if state != Tokenizer.State_None:
+                            result.append((state, token))
+                            state = Tokenizer.State_None
+                        token = ""
+                        i += 1
+                        while expression[i] != data:
+                            token += expression[i]
+                            i += 1
+                        result.append((identf, token))
+                        token = ""
+
+                        parsed = True
+                elif type_ == Tokenizer.Type_Cont_Token:
+                    left, right = data
+                    if char == left:
+                        if state != Tokenizer.State_None:
+                            result.append((state, token))
+                            state = Tokenizer.State_None
+                        token = ""
+
+                        j, sub_result = self._tokenize(expression[i+1:], right)
+                        i += j + 1
+                        result.append((identf, sub_result))
+                        parsed = True
+                if parsed:
+                    break
+            
+            if not parsed:
+                print("Unknown char: " + char)
+
+            i += 1
+        if state != Tokenizer.State_None:
+            result.append((state, token))
+        return i, result
+
+    def tokenize(self, expression: str):
+        return self._tokenize(expression)[1]
+
+
+_special_symbols = ["$", ":", "!", "#", ",", "&"]
 _arithmetic = ["+", "-", "*", "/", "=", "<", ">"]
-_state_arithmetic = 2
 _numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-_state_numbers = 3
 _normal_chars = _numbers + ["_", 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-_state_normal_chars = 4
-
-_state_single_quote_string = 5
-_state_double_quote_string = 6
-
-_state_parenthesis = 7
-_state_curvy_brace = 8
 
 
-
-def tokenize_formula(formula: str, end_char: str="") -> Tuple[int, list]:
-    i = 0
-    length = len(formula)
-    result = []
-    token = ""
-    state = _state_none
-    while i < length:
-        char = formula[i]
-        if char == " ": # skip spaces
-            if state != _state_none:
-                result.append((state, token))
-                state = _state_none
-        elif char == end_char:
-            break
-        elif char == "'":
-            if state != _state_none:
-                result.append((state, token))
-            token = ""
-            state = _state_none
-            i += 1
-            while formula[i] != "'":
-                token += formula[i]
-                i += 1
-            result.append((_state_single_quote_string, token))
-            token = ""
-        elif char == '"':
-            if state != _state_none:
-                result.append((state, token))
-            token = ""
-            state = _state_none
-            i += 1
-            while formula[i] != '"':
-                token += formula[i]
-                i += 1
-            result.append((_state_double_quote_string, token))
-            token = ""
-        elif char == "(":
-            if state != _state_none:
-                result.append((state, token))
-            token = ""
-            state = _state_none
-
-            j, sub_result = tokenize_formula(formula[i+1:], ")")
-            #j += 1
-            i += j + 1
-            result.append((_state_parenthesis, sub_result))
-
-            #print(sub_result)
-            #print(i, j, i+j, formula[i+j], formula)
-            #exit(-1)
-        elif char == "{":
-            if state != _state_none:
-                result.append((state, token))
-            token = ""
-            state = _state_none
-
-            j, sub_result = tokenize_formula(formula[i+1:], "}")
-            i += j + 1
-            result.append((_state_curvy_brace, sub_result))
-        else:
-            if char in _special_symbols:
-                if state != _state_none:
-                    result.append((state, token))
-                token = ""
-                state = _state_none
-                result.append((_state_symbols, char))
-            elif char in _arithmetic:
-                if state != _state_arithmetic:
-                    if state != _state_none:
-                        result.append((state, token))
-                    token = ""
-                    state = _state_arithmetic
-                token += char
-            elif char in _numbers and state != _state_normal_chars:
-                if state != _state_numbers:
-                    if state != _state_none:
-                        result.append((state, token))
-                    token = ""
-                    state = _state_numbers
-                token += char
-            elif char in _normal_chars:
-                if state != _state_normal_chars:
-                    if state != _state_none:
-                        result.append((state, token))
-                    token = ""
-                    state = _state_normal_chars
-                token += char
-        i += 1
-    if state != _state_none:
-        result.append((state, token))
-    return i, result
-
-
-def tokenize_constant(expression: str):
-    return NotImplemented
-
-
-def tokenize(expression: str):
-    if len(expression) == 0:
-        return None
-    if expression[0] == "=":
-        return tokenize_formula(expression[1:])[1]
-    return tokenize_constant(expression)
+formula_tokenizer = Tokenizer()
+formula_tokenizer.register_skipable(" ", "SPACE")
+formula_tokenizer.register_chars_container("'", "SINGLE_QUOTE_STRING")
+formula_tokenizer.register_chars_container('"', "DOUBLE_QUOTE_STRING")
+formula_tokenizer.register_token_container("(", ")", "PARENTHESES")
+formula_tokenizer.register_token_container("{", "}", "CURLY")
+formula_tokenizer.register_token_container("[", "]", "BRACKET")
+formula_tokenizer.register_sequence(_special_symbols, "SYMBOL")
+formula_tokenizer.register_sequence(_arithmetic, "SYMBOL")
+formula_tokenizer.register_sequence(_numbers, "NUMBER")
+formula_tokenizer.register_sequence(_normal_chars, "WORD")
 
 
 if __name__ == "__main__":
     formula = '=IF(OR($B102="", G$3=""), "", CALC_POINTS_FROM_RANGE(M102, M$4:M$203, M$1, someSheet!M$2))'
-    tokenized = tokenize(formula)
+    tokenized = formula_tokenizer.tokenize(formula[1:])
     print(tokenized)
     #print(json.dumps(tokenized, indent=2))
