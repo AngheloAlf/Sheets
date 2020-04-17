d7e3e13adb75aff55e618454e7d6474b6b2ca695
AngheloAlf <angheloalf95@gmail.com>
2020-04-17 12:31:41 -0400

    EarleyItem

--- ASheets/parser/token_analizer.py
+++ ASheets/parser/token_analizer.py
@@ -1,9 +1,58 @@
 from __future__ import annotations
 from typing import List, Tuple
+from typing import Optional
 
 from .token import Token
 from .representations.abstract_representation import ARepresentation
 from .. import utils
+
+
+class EarleyItem():
+    def __init__(self, ruleId=None, name=None, nextPos=None, start=None, end=None, *, item: Optional[EarleyItem]=None):
+        if item is not None:
+            self._ruleId = item.ruleId
+            self._name = item.name
+            self._nextPos = item.nextPos
+            self._start = item.start
+            self._end = item.end
+        else:
+            if ruleId is None or name is None or nextPos is None or start is None:
+                raise ValueError("Invalid parameters.")
+            self._ruleId = ruleId
+            self._name = name
+            self._nextPos = nextPos
+            self._start = start
+            self._end = end
+
+    @property
+    def ruleId(self):
+        return self._ruleId
+    @property
+    def name(self):
+        return self._name
+    @property
+    def nextPos(self):
+        return self._nextPos
+    @property
+    def start(self):
+        return self._start
+    @property
+    def end(self):
+        return self._end
+
+    def __eq__(self, other):
+        if self.ruleId != other.ruleId:
+            return False
+        if self.name != other.name:
+            return False
+        if self.nextPos != other.nextPos:
+            return False
+        if self.start != other.start:
+            return False
+        if self.end != other.end:
+            return False
+        return True
+
 
 class TokenAnalizer():
     def __init__(self):
@@ -24,6 +73,7 @@ class TokenAnalizer():
     def register(self, identifier, sequence: List, callback=None):
         self._registered.append((identifier, (sequence, callback)))
 
+
     def _is_nullable(self, rule, nullables):
         for x in rule[1][0]:
             if x not in nullables:
@@ -42,39 +92,38 @@ class TokenAnalizer():
             old_size = len(nullables)
             self._update_nullables(nullables)
         return nullables
-    
+
+
     def _next_symbol(self, item):
-        if item["next"] >= len(self._registered[item["rule"]]    [1][0]):
+        if item.nextPos >= len(self._registered[item.ruleId]    [1][0]):
             return None
-        return self._registered[item["rule"]]    [1][0]    [item["next"]]
+        return self._registered[item.ruleId]    [1][0]    [item.nextPos]
 
 
     def _complete(self, S, i, j):
         item = S[i][j]
-        for old_i in range(len(S[item["start"]])):
-            old_item = S[item["start"]][old_i]
-            if self._next_symbol(old_item) == item["name"]:
-                aux = {"rule": old_item["rule"], "start": old_item["start"], "next": old_item["next"]+1, "name": old_item["name"]}
+        for old_i in range(len(S[item.start])):
+            old_item = S[item.start][old_i]
+            if self._next_symbol(old_item) == item.name:
+                aux = EarleyItem(old_item.ruleId, old_item.name, old_item.nextPos+1, old_item.start)
                 append_no_repetition(S[i], aux)
-                # S[i].append(aux)
 
     def _scan(self, S, i, j, symbol, tokens):
         item = S[i][j]
         if tokens[i] == symbol:
             if i + 1 >= len(S):
                 S.append([])
-            aux = {"rule": item["rule"], "start": item["start"], "next": item["next"]+1, "name": item["name"]}
+            aux = EarleyItem(item.ruleId, item.name, item.nextPos+1, item.start)
             S[i+1].append(aux)
 
     def _predict(self, S, i, j, symbol, nullables):
         for rule_index in range(len(self._registered)):
             rule_name, rule = self._registered[rule_index]
             if rule_name == symbol:
-                aux = {"rule": rule_index, "start": i, "next": 0, "name": rule_name}
+                aux = EarleyItem(rule_index, rule_name, 0, i)
                 append_no_repetition(S[i], aux)
-                # S[i].append(aux)
                 if rule_name in nullables:
-                    aux = {"rule": S[i][j]["rule"], "start": S[i][j]["start"], "next": S[i][j]["next"]+1, "name": S[i][j]["name"]}
+                    aux = EarleyItem(S[i][j].ruleId, S[i][j].name, S[i][j].nextPos+1, S[i][j].start)
                     append_no_repetition(S[i], aux)
 
 
@@ -82,15 +131,15 @@ class TokenAnalizer():
         nullables = self.get_nullable_rules()
         S = [[]]
         for x in self._starts:
-            S[0].append({"rule": x, "start": 0, "next": 0, "name": self._start})
+            S[0].append(EarleyItem(x, self._start, 0, 0))
         #print(len(tokens))
         #print()
         i = 0
         while i < len(S):
-            #print(i)
+            # print(i)
             j = 0
             while j < len(S[i]):
-                #print("\t", j, end="")
+                # print("\t", j)
                 symbol = self._next_symbol(S[i][j])
                 if symbol == None:
                     #print(" complete")
@@ -106,9 +155,9 @@ class TokenAnalizer():
                     exit(-1)
                 j += 1
             i += 1
-        
         return S
-    
+
+
     def _invert_items(self, items):
         inverted = [[] for i in range(len(items))]
         for i in range(len(items)):
@@ -116,9 +165,9 @@ class TokenAnalizer():
             for y in items[i]:
                 # print("\t", y, self._registered[y["rule"]][1][0])
                 # print("\t", y, y["next"], len(self._registered[y["rule"]][1][0]))
-                if y["next"] == len(self._registered[y["rule"]][1][0]):
-                    y["end"] = i
-                    inverted[y["start"]].append(y)
+                if y.nextPos == len(self._registered[y.ruleId][1][0]):
+                    y._end = i
+                    inverted[y.start].append(y)
                     # print("\t", y, self._registered[y["rule"]][1][0])
             # print()
         return inverted
@@ -127,21 +176,21 @@ class TokenAnalizer():
     def _get_produtions_matching_name_at(self, inverted_items, name, start):
         productions = []
         for item in inverted_items[start]:
-            if item["name"] == name:
+            if item.name == name:
                 productions.append(item)
-        productions.sort(key=lambda x: x["end"]-x["start"], reverse=True)
+        productions.sort(key=lambda x: x.end-x.start, reverse=True)
         return productions
 
     def _test_subproduction(self, inverted_items, subtokens, sub_prod, substart):
-        if sub_prod["end"] - sub_prod["start"] > len(subtokens):
+        if sub_prod.end - sub_prod.start > len(subtokens):
             return None, -1
-        return self._match_subtree(inverted_items, subtokens[substart:substart+sub_prod["end"]-sub_prod["start"]], sub_prod["name"], sub_prod["start"])
+        return self._match_subtree(inverted_items, subtokens[substart:substart+sub_prod.end-sub_prod.start], sub_prod.name, sub_prod.start)
 
 
     def _match_subtree(self, inverted_items, subtokens, actual_name, start=0):
         items_with_actual_name = self._get_produtions_matching_name_at(inverted_items, actual_name, start)
         for item in items_with_actual_name:
-            rules = self._registered[item["rule"]][1][0]
+            rules = self._registered[item.ruleId][1][0]
             substart = 0
             matchs = True
             data = []
@@ -164,7 +213,6 @@ class TokenAnalizer():
                             data.append(subdata)
                             substart += sub_substart
                             break
-                    # substart += len(productions)
             if matchs:
                 return (actual_name(rules, data), substart)
         return (None, -1)
