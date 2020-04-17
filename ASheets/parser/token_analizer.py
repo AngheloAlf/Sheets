75ede42522cbb097fdb2aa1d61a41490ef700ef5
AngheloAlf <angheloalf95@gmail.com>
2020-04-17 16:10:06 -0400

    Analizer: add some types to signatures

--- ASheets/parser/token_analizer.py
+++ ASheets/parser/token_analizer.py
@@ -1,20 +1,27 @@
 from __future__ import annotations
 from typing import List, Tuple
-from typing import Optional
+from typing import Type, Optional
 
 from .token import Token
 from .representations.abstract_representation import ARepresentation
 from .. import utils
 
 
+class RuleSequence():
+    def __init__(self, name: Type[ARepresentation], sequence: List, callback):
+        self._name = name
+        self._sequence = sequence
+        self._callback = callback
+
+
 class EarleyItem():
-    def __init__(self, ruleId: Optional[int]=None, name=None, nextPos: int=0, start: int=0, end=None, *, item: Optional[EarleyItem]=None):
+    def __init__(self, ruleId: Optional[int]=None, name: Optional[Type[ARepresentation]]=None, nextPos: int=0, start: int=0, end=None, *, item: Optional[EarleyItem]=None):
         if item is not None:
-            self._ruleId = item.ruleId
-            self._name = item.name
-            self._nextPos = item.nextPos
-            self._start = item.start
-            self._end = item.end
+            self._ruleId: int = item.ruleId
+            self._name: Type[ARepresentation] = item.name
+            self._nextPos: int = item.nextPos
+            self._start: int = item.start
+            self._end: int = item.end
         else:
             if ruleId is None or name is None:
                 raise ValueError("Invalid parameters.")
@@ -25,19 +32,19 @@ class EarleyItem():
             self._end = end
 
     @property
-    def ruleId(self):
+    def ruleId(self) -> int:
         return self._ruleId
     @property
-    def name(self):
+    def name(self) -> Type[ARepresentation]:
         return self._name
     @property
-    def nextPos(self):
+    def nextPos(self) -> int:
         return self._nextPos
     @property
-    def start(self):
+    def start(self) -> int:
         return self._start
     @property
-    def end(self):
+    def end(self) -> int:
         return self._end
 
     def __eq__(self, other):
@@ -68,7 +75,7 @@ class EarleyStateSet():
     def __getitem__(self, key):
         return self._items[key]
     
-    def newItem(self, ruleId: int, name, *, unique: bool=False):
+    def newItem(self, ruleId: int, name: Type[ARepresentation], *, unique: bool=False):
         item = EarleyItem(ruleId, name, start=self._setId)
         if unique and item in self._items:
             return
@@ -89,14 +96,14 @@ class EarleyStateSet():
 
 
 class EarleyParser():
-    def __init__(self, rules, startName, startsIndexes):
+    def __init__(self, rules, startName: Type[ARepresentation], startsIndexes: List[int]):
         self._rules = rules
         self._S: List[EarleyStateSet] = [EarleyStateSet(0)]
         for rule_index in startsIndexes:
             self._S[0].newItem(rule_index, startName)
 
 
-    def _is_nullable(self, rule, nullables):
+    def _is_nullable(self, rule, nullables: set) -> bool:
         for x in rule[1]:
             if x not in nullables:
                 return False
@@ -106,6 +113,7 @@ class EarleyParser():
         for i in range(len(self._rules)):
             if self._is_nullable(self._rules[i], nullables):
                 nullables.add(self._rules[i][0])
+        return
 
     def get_nullable_rules(self):
         nullables = set()
@@ -123,7 +131,7 @@ class EarleyParser():
         return sequence[item.nextPos]
 
 
-    def _complete(self, i, j, actual_item: EarleyItem):
+    def _complete(self, i: int, actual_item: EarleyItem):
         start_set_of_item = self._S[actual_item.start]
         for old_i in range(len(start_set_of_item)):
             old_item = start_set_of_item[old_i]
@@ -131,14 +139,14 @@ class EarleyParser():
                 self._S[i].addItemNext(old_item, unique=True)
         return
 
-    def _scan(self, i, j, symbol, tokens, actual_item: EarleyItem):
+    def _scan(self, i: int, symbol, tokens: List, actual_item: EarleyItem):
         if tokens[i] == symbol:
             if i + 1 >= len(self._S):
                 self._S.append(EarleyStateSet(i+1))
             self._S[i+1].addItemNext(actual_item)
         return
 
-    def _predict(self, i, j, symbol, actual_item: EarleyItem, nullables):
+    def _predict(self, i: int, symbol: Type[ARepresentation], actual_item: EarleyItem, nullables):
         for rule_index in range(len(self._rules)):
             rule_name, rule, callback = self._rules[rule_index]
             if rule_name == symbol:
@@ -148,7 +156,7 @@ class EarleyParser():
         return
 
 
-    def _invert_items(self):
+    def _invert_items(self) -> List[EarleyStateSet]:
         inverted: List[EarleyStateSet] = [EarleyStateSet(i) for i in range(len(self._S))]
         for i in range(len(self._S)):
             for y in self._S[i]:
@@ -168,11 +176,11 @@ class EarleyParser():
                 actual_item = state_set[j]
                 symbol = self._next_symbol(actual_item)
                 if symbol == None:
-                    self._complete(i, j, actual_item)
+                    self._complete(i, actual_item)
                 elif is_terminal(symbol):
-                    self._scan(i, j, symbol, tokens, actual_item)
+                    self._scan(i, symbol, tokens, actual_item)
                 elif is_non_terminal(symbol):
-                    self._predict(i, j, symbol, actual_item, nullables)
+                    self._predict(i, symbol, actual_item, nullables)
                 else:
                     print("Wat?")
                     exit(-1)
@@ -184,7 +192,7 @@ class EarleyParser():
 
 class TokenAnalizer():
     def __init__(self):
-        self._start = None
+        self._start: Optional[Type[ARepresentation]] = None
         self._starts: List[int] = list()
         self._registered = list()
 
@@ -193,33 +201,13 @@ class TokenAnalizer():
     def __repr__(self):
         return "<TokenAnalizer>"
 
-    def register_start(self, identifier, sequence: List, callback=None):
+    def register_start(self, identifier: Type[ARepresentation], sequence: List, callback=None):
         self._start = identifier
         self._starts.append(len(self._registered))
         self.register(identifier, sequence, callback)
 
     def register(self, identifier, sequence: List, callback=None):
         self._registered.append((identifier, sequence, callback))
-
-
-    def _is_nullable(self, rule, nullables):
-        for x in rule[1]:
-            if x not in nullables:
-                return False
-        return True
-
-    def _update_nullables(self, nullables):
-        for i in range(len(self._registered)):
-            if self._is_nullable(self._registered[i], nullables):
-                nullables.add(self._registered[i][0])
-
-    def get_nullable_rules(self):
-        nullables = set()
-        old_size = -1
-        while old_size != len(nullables):
-            old_size = len(nullables)
-            self._update_nullables(nullables)
-        return nullables
 
 
     def _get_produtions_matching_name_at(self, inverted_items, name, start):
@@ -256,7 +244,6 @@ class TokenAnalizer():
                     matchs = False
                     for sub_prod in productions:
                         subdata, sub_substart = self._test_subproduction(inverted_items, subtokens, sub_prod, substart)
-                        # print(subdata)
                         if subdata is not None:
                             matchs = True
                             data.append(subdata)
@@ -268,13 +255,13 @@ class TokenAnalizer():
 
 
     def parse(self, tokens: List[Token]):
+        if self._start is None:
+            raise RuntimeError("Start point has not been setted.")
         parser = EarleyParser(self._registered, self._start, self._starts)
         items = parser.parse(tokens)
-        # print(len(items), len(tokens)+1)
         if len(items) != len(tokens)+1:
             return None
         tree = self._match_subtree(items, tokens, self._start)
-        # print(tree)
         return tree[0]
 
 
